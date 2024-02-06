2c0aa612b3113d61b91a85dfdc7756a5c454be4b
NicanorArg <150375931+NicanorArg@users.noreply.github.com>
2024-02-05 21:22:14 -0300

    Ejercicio 2 resuelto

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
@@ -5,8 +5,9 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Nicanor
+apellido: Gatti
+Tutora: Natali
 ---
 Ejercicio: entrada_salida_02
 ---
@@ -27,8 +28,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
-        
+        nombre = prompt("Nombre", "Ingrese su nombre")
+        alert("Nombre", nombre)
         
     
 if __name__ == "__main__":
